47d1c8d8dfa61218d4942aa527f7afb493cf0c0b
JinSeop <turn3361@gmail.com>
2020-07-20 17:24:38 +0900

    서버 완성

--- server/index.js
+++ server/index.js
@@ -1,32 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-const fs = require('fs')
-const path = require('path')
-const app = express()
-const port = 4000
+const express = require('express');
+const path = require('path');
 
-const emailListPath = path.join(__dirname,'/public','emailList.json')
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+const PORT = 4000;
 
-app.post('/email',(req,res) => {
-  fs.readFile(emailListPath,'utf8',(err,data) => {
-    data = JSON.parse(data)
-    data.push(req.body)
-    const temp = JSON.stringify(data)
-    fs.writeFile(emailListPath,temp,(err) =>{
-      if(err){
-        res.status(500).send('Error')
-      }
-      res.status(201).send()
-    })
-  })
-})
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(function(req, res, next) {
-  res.status(404).send('Sorry cant find that!');
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(PORT, () => {
+  console.log(`server is listening on http://localhost:${PORT}`);
+});
---
+++ server/public/precache-manifest.74d89affeaed18e264c64a3ac56f09b6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da7a3bf9795bef46ca22384b7e74522",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fbf4f3d912b5b67d5b25",
+    "url": "./static/css/main.4931e670.chunk.css"
+  },
+  {
+    "revision": "f66e7b591e03624bd9b8",
+    "url": "./static/js/2.6271650c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6271650c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf4f3d912b5b67d5b25",
+    "url": "./static/js/main.e841b82b.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "13d93e0c77e8858c355d7c608bd41c33",
+    "url": "./static/media/back.13d93e0c.png"
+  },
+  {
+    "revision": "16887f3634dfc7ba55d322694673a501",
+    "url": "./static/media/burger.16887f36.png"
+  },
+  {
+    "revision": "f90d5832401d2705d259b7bea9b264e8",
+    "url": "./static/media/css.f90d5832.png"
+  },
+  {
+    "revision": "26bdcd1522937d05915328db94148a7a",
+    "url": "./static/media/express.26bdcd15.png"
+  },
+  {
+    "revision": "8931588562ea96c1e24485d029eb6ed0",
+    "url": "./static/media/html.89315885.png"
+  },
+  {
+    "revision": "89b2ab4cbe1e66777ab3cefeff4d86a4",
+    "url": "./static/media/img1.89b2ab4c.png"
+  },
+  {
+    "revision": "e56e37dea453afc79e0a029d0c615488",
+    "url": "./static/media/js.e56e37de.png"
+  },
+  {
+    "revision": "d55ac346ee5b02ce35cdb123875e439e",
+    "url": "./static/media/mysql.d55ac346.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "./static/media/nodejs.55e83371.png"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "./static/media/react.f297dfcd.png"
+  }
+]);
