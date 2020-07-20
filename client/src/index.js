53ec116acce76576150097649061d7e20c0d9e7c
JinSeop <turn3361@gmail.com>
2020-07-20 17:25:13 +0900

    클라 완성

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './components/App';
 
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+
+  ReactDOM.hydrate(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+  )
 
 
